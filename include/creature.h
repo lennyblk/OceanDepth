cadc6a78a6b199de889b70f2bcab5c75eeba19a4
Erwan <44072181+Erwanlg0@users.noreply.github.com>
2025-10-09 09:34:46 +0200

    Refactor creature generation and update API

--- include/creature.h
+++ include/creature.h
@@ -2,18 +2,16 @@
 #define CREATURE_H
 
 #include "types.h"
+#include <stddef.h>
 
-// création et gestion des créatures
-Creature create_creature(CreatureType type, int zone_level);
-void initialize_creature_stats(Creature *creature);
-int get_creature_spawn_chance(int zone, CreatureType type);
-Creature* generate_random_creatures(int zone, int *count);
-const char* get_creature_name(CreatureType type);
-const char* get_creature_emoji(CreatureType type);
-void display_creature_info(Creature *creature);
+Creature create_creature(CreatureType type);
 
-// Fonctions utilitaires
-int calculate_creature_level(int zone);
-int is_creature_boss(CreatureType type);
+void generate_creatures(int zone_depth, Creature creature_array[], size_t array_size, size_t *generated_count);
+
+const char *get_creature_name(CreatureType type);
+
+const char *get_creature_emoji(CreatureType type);
+
+void display_creature_info(const Creature *creature);
 
 #endif
