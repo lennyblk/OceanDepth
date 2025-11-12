7f8ab22fc22b4ed7292cc98e8878f104e0775dd1
Erwan <44072181+Erwanlg0@users.noreply.github.com>
2025-11-12 11:36:29 +0100

    Fix reverts merge
    ???

---
+++ include/creature.h
@@ -0,0 +1,17 @@
+#ifndef CREATURE_H
+#define CREATURE_H
+
+#include "types.h"
+#include <stddef.h>
+
+Creature create_creature(CreatureType type, int zone_level);
+
+void generate_creatures(int zone_depth, Creature creature_array[], size_t array_size, size_t *generated_count);
+
+const char *get_creature_name(CreatureType type);
+
+const char *get_creature_emoji(CreatureType type);
+
+void display_creature_info(const Creature *creature);
+
+#endif
